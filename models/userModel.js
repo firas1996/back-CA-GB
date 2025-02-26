94b3d800358785dd8e61b57dd1b9831f7d3fd19c
firas hamila <firas.hm.1996@gmail.com>
2025-02-26 21:01:32 +0100

    a

---
+++ models/userModel.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const validator = require("validator");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "The name is required !!!"],
+  },
+  email: {
+    type: String,
+    required: [true, "The email is required !!!"],
+    unique: [true, "This email is already used !!!"],
+    validate: [validator.isEmail, "This email is not valid !!!"],
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "The password is required !!!"],
+    minlength: 6,
+  },
+  confirmPassword: {
+    type: String,
+    required: [true, "The password is required !!!"],
+    minlength: 6,
+    validate: {
+      validator: function (cPass) {
+        return cPass === this.password;
+      },
+      message: "password and cPass does not match !!!!",
+    },
+  },
+  role: {
+    type: String,
+    enum: ["user", "admin"],
+    default: "user",
+  },
+  last_updated_password_date: {
+    type: Date,
+    default: Date.now(),
+  },
+  created_at: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
