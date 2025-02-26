8416121dee04c96f58b1301a8d187002e2cf60a5
firas hamila <firas.hm.1996@gmail.com>
2025-02-26 20:19:17 +0100

    a

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
 const app = express();
-
 const port = 5000;
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("DB connection secured !!!");
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(port, () => {
   try {
