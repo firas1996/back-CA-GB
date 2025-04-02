e545d89a12d70451552073cc0b4e24620d2ec747
firas hamila <firas.hm.1996@gmail.com>
2025-04-02 21:49:24 +0200

    aa

--- controllers/userController.js
+++ controllers/userController.js
@@ -46,6 +46,35 @@ exports.getUserById = async (req, res) => {
   }
 };
 
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body);
+    res.status(200).json({
+      message: "Success !!!!",
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error.message,
+    });
+  }
+};
+
+exports.deleteUser = async (req, res) => {
+  try {
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      message: "Success !!!!",
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error.message,
+    });
+  }
+};
+
 exports.getUserByName = async (req, res) => {
   try {
     const user = await User.find({ name: req.params.name });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,11 +3,15 @@ const {
   createUser,
   getAllUsers,
   getUserById,
+  updateUser,
+  deleteUser,
 } = require("../controllers/userController");
 const Router = express.Router();
 
 Router.post("/", createUser);
 Router.get("/", getAllUsers);
 Router.get("/:id", getUserById);
+Router.patch("/:id", updateUser);
+Router.delete("/:id", deleteUser);
 
 module.exports = Router;
