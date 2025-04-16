4a575e6379ac89e214f8d622150d4a379f37952e
firas hamila <firas.hm.1996@gmail.com>
2025-04-16 19:49:02 +0200

    aa

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const userRoutes = require("./routes/userRoutes");
 dotenv.config({ path: "./.env" });
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use("/users", userRoutes);
 const port = 5000;
 
